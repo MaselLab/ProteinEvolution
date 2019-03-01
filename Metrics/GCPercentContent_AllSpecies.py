387199a880d6a1a0de8c581399f15b3e1465fa8a
SWillis-MaselLabs <sarawillis@email.arizona.edu>
2019-03-01 13:52:57 -0700

    Update GCPercentContent_AllSpecies.py

--- Metrics/GCPercentContent_AllSpecies.py
+++ Metrics/GCPercentContent_AllSpecies.py
@@ -3,8 +3,10 @@ import numpy as np
 
 
 '''
-Author : Sara Willis
-Date   : February 18, 2019
+Author   : Sara Willis
+Date     : February 18, 2019
+Modified : March 1, 2019 by Sara Willis
+-- This script initially calculated the GC content by dividing the number of G's + C's by the total number of nucleotides. It makes more sense to divide by the sum of the four nucleotides so we exclude unknown nucleic acids from our calculations.
 
 The purpose of this script is to calculate the GC content for all species in a MySQL database using their coding sequences. The results are then uploaded to be stored with the relevant species
 
@@ -50,7 +52,7 @@ for SequenceDatabase in Databases:
 
     for i in range(MinSpeciesUID,MaxSpeciesUID):
         # GC content is the fraction of G's and C's to all nucleotides
-        FullLength = 0
+        AllKnownNucleicAcids = 0
         SumOfGsAndCs = 0
         
         mycursor.execute("SELECT CodingSequence FROM " + NucleotideSequenceDatabase + " WHERE SpeciesUID = %s"%i)
@@ -60,10 +62,10 @@ for SequenceDatabase in Databases:
         else:
             for result in results:
                 NucleotideSequence = result[0]
-                FullLength += len(NucleotideSequence)
+                AllKnownNucleicAcids += (NucleotideSequence.count('C') + NucleotideSequence.count('G') + NucleotideSequence.count('A') + NucleotideSequence.count('T'))
                 SumOfGsAndCs += (NucleotideSequence.count('C') + NucleotideSequence.count('G'))
 
-            FractionOfGC = '%.4f'%(SumOfGsAndCs/FullLength)
+            FractionOfGC = '%.4f'%(SumOfGsAndCs/AllKnownNucleicAcids)
             # Once the GC content is found, we store it in a species database for later use
             mycursor.execute("UPDATE SpeciesList SET GCContent=%s WHERE SpeciesUID = %s",(FractionOfGC,i))
             cnx.commit()
