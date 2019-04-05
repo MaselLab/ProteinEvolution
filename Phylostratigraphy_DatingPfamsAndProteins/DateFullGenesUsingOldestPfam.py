0564a79353b0deb80b1b6acff212dace5783f3d7
SWillis-MaselLabs <sarawillis@email.arizona.edu>
2019-04-05 16:49:33 -0700

    Add files via upload

--- Phylostratigraphy_DatingPfamsAndProteins/DateFullGenesUsingOldestPfam.py
+++ Phylostratigraphy_DatingPfamsAndProteins/DateFullGenesUsingOldestPfam.py
@@ -45,15 +45,28 @@ Password = ''
 
 # Protein Data Tables (contains relevant Pfam UIDs)
 EnsemblProteinDataTable = 'EnsemblGenomes_Protein_Complete'
+EnsemblUIDColumn = 'UID'
+EnsemblPfamUIDColumn = 'PfamUID'
+
 NCBIProteinDataTable = 'NCBIGenomes_Protein_Complete'
+NCBIUIDColumn = 'UID'
+NCBIPfamUIDColumn = 'PfamUID'
 
 # Protein Metrics Data Table (where the protein ages should be stored)
 EnsemblProteinMetricsDataTable = 'EnsemblGenomes_ProteinMetrics_Complete'
+EnsemblAgeOfOldestPfamColumn = 'AgeOfOldestPfam_LUCA_LECA_Updated'
+EnsemblProteinTableUID = 'ProteinTableUID'
+
 NCBIProteinMetricsDataTable = 'NCBIGenomes_ProteinMetrics_Complete'
+NCBIAgeOfOldestPfamColumn = 'AgeOfOldestPfam_LUCA_LECA_Updated'
+NCBIProteinTableUID = 'ProteinTableUID'
 
 # Pfam Data Table
 PfamDataTable = 'PfamUIDsTable_EnsemblAndNCBI'
+PfamUIDColumn = 'PfamUID'
+PfamAgeColumn = 'Age_Oldest_MY'
 
+Verbose = True
 
 
 ########################################################################
@@ -71,13 +84,18 @@ mycursor = cnx.cursor(buffered = True)
 
 
 # The protein table UID and the pfam UIDs associated with that protein are extracted from the Ensembl database
-print('Extracting Entries from Ensembl Protein Data Table\n\n')
-SelectProteins_Ensembl_Statement = "SELECT UID,PfamUID FROM " + EnsemblProteinDataTable
+if Verbose == True:
+    print('Extracting Entries from Ensembl Protein Data Table\n\n')
+    sys.stdout.flush()
+
+SelectProteins_Ensembl_Statement = "SELECT "+','.join([EnsemblUIDColumn,EnsemblPfamUIDColumn])+" FROM " + EnsemblProteinDataTable
 mycursor.execute(SelectProteins_Ensembl_Statement)
 EnsemblResults = mycursor.fetchall()
 
 # The process of dating the genes starts once the extraction is complete
-print('Ensembl Entries Extracted\nTime Taken: %s\n\nDating Ensembl Proteins\n\n'%(datetime.datetime.now()-startTime))
+if Verbose == True:
+    print('Ensembl Entries Extracted\nTime Taken: %s\n\nDating Ensembl Proteins\n\n'%(datetime.datetime.now()-startTime))
+    sys.stdout.flush()
 currentTime = datetime.datetime.now()
 for result in EnsemblResults:
     UID = result[0]
@@ -86,22 +104,24 @@ for result in EnsemblResults:
     # The pfam ages associated with that protein are then extracted from the pfam table.
     # The ages are iterated through and the oldest is selected
     for pfam in Pfams:
-        SelectPfamStatement = "SELECT Age_MY FROM " + PfamDataTable + " WHERE PfamUID = '%s'"%pfam
+        SelectPfamStatement = "SELECT "+PfamAgeColumn+" FROM " + PfamDataTable + " WHERE "+PfamUIDColumn+" = "+pfam
         mycursor.execute(SelectPfamStatement)
         Age = mycursor.fetchone()[0]
         if Age >= OldestPfamAge:
             OldestPfamAge = copy.deepcopy(Age)
     # Once the oldest pfam is found, that age is assigned to the protein. That age is then uploaded
     # to the protein metrics table
-    UpdateProteinFullAgeStatement = "UPDATE " + EnsemblProteinMetricsDataTable + " SET AgeOfOldestPfam=%s WHERE ProteinTableUID = %s"%(OldestPfamAge,UID)
+    UpdateProteinFullAgeStatement = "UPDATE " + EnsemblProteinMetricsDataTable + " SET "+EnsemblAgeOfOldestPfamColumn+"="+OldestPfamAge+" WHERE "+EnsemblProteinTableUID+" ="+str(UID)
     mycursor.execute(UpdateProteinFullAgeStatement)
     cnx.commit()
 
-print('Ensembl Protein Table Updated\nTime Taken: %s\n\nExtracting Entries from NCBI Protein Data Table\n\n'%(datetime.datetime.now()-currentTime))
+if Verbose == True:
+    print('Ensembl Protein Table Updated\nTime Taken: %s\n\nExtracting Entries from NCBI Protein Data Table\n\n'%(datetime.datetime.now()-currentTime))
+    sys.stdout.flush()
 currentTime = datetime.datetime.now()
 
 # Once the Ensembl proteins are dated, the NCBI proteins go through the same process
-SelectProteins_NCBI_Statement = "SELECT UID,PfamUID FROM " + NCBIProteinDataTable
+SelectProteins_NCBI_Statement = "SELECT "+','.join([NCBIUIDColumn,NCBIPfamUIDColumn])+" FROM " + NCBIProteinDataTable
 mycursor.execute(SelectProteins_NCBI_Statement)
 NCBIResults = mycursor.fetchall()
 print('NCBI Entries Extracted\nTime Taken: %s\n\nDating NCBI Proteins' %(datetime.datetime.now()-currentTime))
@@ -111,14 +131,18 @@ for result in NCBIResults:
     Pfams = result[1].split(',')
     OldestPfamAge = 0
     for pfam in Pfams:
-        SelectPfamStatement = "SELECT Age_MY FROM " + PfamDataTable + " WHERE PfamUID = '%s'"%pfam
+        SelectPfamStatement = "SELECT "+PfamAgeColumn+" FROM " + PfamDataTable + " WHERE "+PfamUIDColumn+" = "+pfam
         mycursor.execute(SelectPfamStatement)
         Age = mycursor.fetchone()[0]
         if Age >= OldestPfamAge:
             OldestPfamAge = copy.deepcopy(Age)
-    UpdateProteinFullAgeStatement = "UPDATE " + NCBIProteinMetricsDataTable + " SET AgeOfOldestPfam=%s WHERE ProteinTableUID = %s"%(OldestPfamAge,UID)
+    UpdateProteinFullAgeStatement = "UPDATE " + NCBIProteinMetricsDataTable + " SET "+NCBIAgeOfOldestPfamColumn+"="+OldestPfamAge+" WHERE "+NCBIProteinTableUID+" = "+str(UID)
     mycursor.execute(UpdateProteinFullAgeStatement)
     cnx.commit()
-print('NCBI Protein Table Updated\nTime Take: %s\n\nProgram Complete!\n\nTotal Time Taken: %s'%(datetime.datetime.now()-currentTime, datetime.datetime.now()-startTime))
+if Verbose == True:
+    print('NCBI Protein Table Updated\nTime Take: %s\n\nProgram Complete!\n\nTotal Time Taken: %s'%(datetime.datetime.now()-currentTime, datetime.datetime.now()-startTime))
+    sys.stdout.flush()
 
 cnx.close()
+
+print('\nScript Complete\nTime Taken: %s'%(datetime.datetime.now()-startTime))
